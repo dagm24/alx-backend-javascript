4737eca99f87effaea0eca7a101627f46a4aa4fb
dagmawit terefe <dagm24b@gmail.com>
2025-02-07 00:25:31 +0300

    modified

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -34,7 +34,6 @@ const readDatabase = async (dataPath) => {
         propName,
         studentPropValues[idx],
       ]);
-
       studentGroups[field].push(Object.fromEntries(studentEntries));
     }
 
