d3b8e17fb6bda44f0119e5c7eef9f720b05758ac
dagmawit terefe <dagm24b@gmail.com>
2025-02-07 22:34:21 +0300

    fix

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,5 +1,5 @@
-import express from "express";
-import mapRoutes from "./routes/index.js";
+import express from 'express';
+import mapRoutes from './routes/index';
 
 const app = express();
 const PORT = 1245;
